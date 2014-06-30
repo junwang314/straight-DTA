ae728e9ed6e3217bdcc8890fb79724e31868017f
jun <jun@jun-PowerEdge-T310.(none)>
2014-06-29 20:39:44 -0400

    genFile added, can generate file of different sizes

---
+++ genFile/genFile.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(int argc, char** argv)
+{
+	if(argc<2)
+	{
+		printf("usage: genFile fileSize \n");
+		exit(0);
+	}
+	int size=0;
+	void* buf=malloc(1024*1024);
+	FILE* outF;
+	for(int i=1; i<argc; i++)
+	{
+		char fName[30];
+		size=atoi(argv[i]);
+		sprintf(fName,"file_%dMB\0",size);
+		printf("generat %s\n",fName);
+		outF=fopen(fName,"w+");
+		for(int j=0; j<size*8; j++)
+		{
+			fwrite(outF, 1024, 1024,outF);
+		}
+		fclose(outF);
+	}
+	return 1;
+}
+
