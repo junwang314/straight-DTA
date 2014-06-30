dfaf09d313816907e66a8a28ce3333b239f8bdcc
xgy <xgy@xgy-XPS-8700.(none)>
2014-06-29 22:48:35 -0400

    sshbench modified

--- sshdBench/run.sh
+++ sshdBench/run.sh
@@ -1,7 +1,10 @@
-scp ../genFile/file_1MB jun@130.203.157.226:/root/file_1MB
-scp ../genFile/file_5MB jun@130.203.157.226:/root/file_5MB
-scp ../genFile/file_10MB jun@130.203.157.226:/root/file_10MB
-scp ../genFile/file_20MB jun@130.203.157.226:/root/file_20MB
-scp ../genFile/file_50MB jun@130.203.157.226:/root/file_50MB
-scp ../genFile/file_100MB jun@130.203.157.226:/root/file_100MB
-
+for i in 1
+do
+	echo $i
+	scp ../genFile/file_1MB root@130.203.157.226:/root/file_1MB
+	scp ../genFile/file_5MB root@130.203.157.226:/root/file_5MB
+	scp ../genFile/file_10MB root@130.203.157.226:/root/file_10MB
+	scp ../genFile/file_20MB root@130.203.157.226:/root/file_20MB
+	scp ../genFile/file_50MB root@130.203.157.226:/root/file_50MB
+	scp ../genFile/file_100MB root@130.203.157.226:/root/file_100MB
+done
