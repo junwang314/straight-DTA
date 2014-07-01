d7014b99ad6c4ccff3a9c6e3bd5f6258a9b99e51
xgy <xgy@xgy-XPS-8700.(none)>
2014-06-30 21:34:09 -0400

    curl conf files aupdated

--- vsftpd-2.2.2/test/client.py
+++ vsftpd-2.2.2/test/client.py
@@ -5,9 +5,9 @@ argv=sys.argv[1]
 argv=int(argv)
 i=0
 while i<argv:
-    #ftp = FTP("130.203.157.226", "jun", "123")
-    ftp = FTP("localhost", "jun", "123")
-    ftp.cwd("/home/jun/straight-DTA/vsftpd-2.2.2/test")
+    ftp = FTP("130.203.157.226", "jun", "123")
+   # ftp = FTP("localhost", "jun", "123")
+    ftp.cwd("straight-DTA/vsftpd-2.2.2/test")
 #    print i
     for j in range(100):
         with open('client.txt') as f:
