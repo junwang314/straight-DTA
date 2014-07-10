51201829584677df9a4998f5532d170b5e977ff7
ShawnXiao2007 <windxing2007@gmail.com>
2014-07-10 17:22:06 -0400

    client_vm, client_jun updated

--- vsftpd-2.2.2/test/client_jun.py
+++ vsftpd-2.2.2/test/client_jun.py
@@ -1,8 +1,6 @@
 from ftplib import FTP 
 import sys
 
-argv=sys.argv[1]
-argv=int(argv)
 #    ftp = FTP("130.203.157.226", "jun", "123")
 ftp = FTP("localhost", "jun", "123")
 ftp.cwd("straight-DTA/vsftpd-2.2.2/test")
