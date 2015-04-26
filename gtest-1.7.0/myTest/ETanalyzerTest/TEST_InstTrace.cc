6d468fb6978184ec72bb8b0b5e935f38cf6aec52
ShawnXiao2007 <windxing2007@gmail.com>
2015-04-25 23:30:43 -0400

    Finishing ForkQueu Test

--- gtest-1.7.0/myTest/ETanalyzerTest/TEST_InstTrace.cc
+++ gtest-1.7.0/myTest/ETanalyzerTest/TEST_InstTrace.cc
@@ -1,5 +1,9 @@
 #include "gtest/gtest.h"
 //for ET-analyzer
+#include "/home/jun/llvm-3.x/llvm/lib/Transforms/Hello/common.h"
+#include "/home/jun/llvm-3.x/llvm/lib/Transforms/Hello/DEBUG_utils.h"
+#include "/home/jun/llvm-3.x/llvm/lib/Transforms/Hello/INPUT_SigHandler.h"
+#include "/home/jun/llvm-3.x/llvm/lib/Transforms/Hello/INPUT_GlobalData.h"
 #include "/home/jun/llvm-3.x/llvm/lib/Transforms/Hello/INPUT_InstTrace.h"
 //for load module from file
 #include </home/jun/llvm-3.x/llvm/include/llvm/IR/Module.h>
@@ -12,6 +16,10 @@
 
 using namespace std;
 using namespace llvm;
+
+shared_ptr<INPUT_GlobalData> globalData;
+shared_ptr<INPUT_SigHandlerSet> sigHandlerSet;
+shared_ptr<DEBUG_utils> debug_utils;
 
 //declare test fixture
 class InstTraceFix: public testing::Test{
@@ -26,8 +34,41 @@ class InstTraceFix: public testing::Test{
 };
 
 TEST_F(InstTraceFix, ForkQueue){
-  EXPECT_TRUE(true);
+	string logdir("/home/jun/straight-DTA/pserv-3.3/test");
+	string cfigfile("configFile");
+
+	debug_utils.reset(new DEBUG_utils());
+	sigHandlerSet.reset(new INPUT_SigHandlerSet( (logdir+"/"+"sigHandlerFile").c_str() ));
+	globalData.reset(new INPUT_GlobalData(*m1));
+
+	INPUT_InstTrace* it=new INPUT_InstTrace( *m1, logdir, cfigfile );
+	EXPECT_FALSE(it->isForkQueueEmpty());
+	char* fq1=it->getForkQueueFront();
+	EXPECT_TRUE(fq1!=NULL);
+	string fq1s(fq1);
+	EXPECT_TRUE(fq1s == "tmp.19007.full");
+	it->popForkQueue();
+	EXPECT_FALSE(it->isForkQueueEmpty());
+	char* fq2=it->getForkQueueFront();
+	EXPECT_TRUE(fq2!=NULL);
+	string fq2s(fq2);
+	EXPECT_TRUE(fq2s == "tmp.19010.full");
+	it->popForkQueue();
+	EXPECT_TRUE(it->isForkQueueEmpty());
+
+	delete it;
+	debug_utils.reset((DEBUG_utils*)NULL);
+	sigHandlerSet.reset((INPUT_SigHandlerSet*)NULL);
+	globalData.reset((INPUT_GlobalData*)NULL);
 }
+
+/*
+TEST_F(InstTraceFix, traceBB){
+	string cfpath("/home/jun/straight-DTA/pserv-3.3/test/configFile");
+	INPUT_InstTrace* it=new INPUT_InstTrace( *m1, cfpath );
+	EXPECT_FALSE(it->isForkQueueEmpty());
+}
+*/
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
