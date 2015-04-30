4c09a93de199a37b9d7f590d21ccb58f7e21424f
ShawnXiao2007 <windxing2007@gmail.com>
2015-04-30 09:54:23 -0400

    Done TEST_InstTrace:ForkQueue and traceBB

--- gtest-1.7.0/myTest/ETanalyzerTest/TEST_InstTrace.cc
+++ gtest-1.7.0/myTest/ETanalyzerTest/TEST_InstTrace.cc
@@ -30,7 +30,7 @@ class InstTraceFix: public testing::Test{
 		}
     LLVMContext context1;
 		SMDiagnostic error1;
-		Module * m1;
+		Module const * m1;
 };
 
 TEST_F(InstTraceFix, ForkQueue){
@@ -54,21 +54,33 @@ TEST_F(InstTraceFix, ForkQueue){
 	string fq2s(fq2);
 	EXPECT_TRUE(fq2s == "tmp.19010.full");
 	it->popForkQueue();
-	EXPECT_TRUE(it->isForkQueueEmpty());
-
+	EXPECT_TRUE(it->isForkQueueEmpty());	
+	
 	delete it;
+
 	debug_utils.reset((DEBUG_utils*)NULL);
 	sigHandlerSet.reset((INPUT_SigHandlerSet*)NULL);
 	globalData.reset((INPUT_GlobalData*)NULL);
 }
 
-/*
+
 TEST_F(InstTraceFix, traceBB){
-	string cfpath("/home/jun/straight-DTA/pserv-3.3/test/configFile");
-	INPUT_InstTrace* it=new INPUT_InstTrace( *m1, cfpath );
+	string logdir("/home/jun/straight-DTA/pserv-3.3/test");
+	string cfigfile("configFile");
+
+	debug_utils.reset(new DEBUG_utils());
+	sigHandlerSet.reset(new INPUT_SigHandlerSet( (logdir+"/"+"sigHandlerFile").c_str() ));
+	globalData.reset(new INPUT_GlobalData(*m1));
+
+	INPUT_InstTrace* it=new INPUT_InstTrace( *m1, logdir, cfigfile );
+
 	EXPECT_FALSE(it->isForkQueueEmpty());
+	char const* traceFileName=it->getLogFileName();
+	it->updateInstTrace(traceFileName);
+	EXPECT_TRUE(it->isForkQueueEmpty());
+
 }
-*/
+
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
